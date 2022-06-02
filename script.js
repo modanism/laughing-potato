4a70491f6bf2694e4d4b7acca07562749457e951
modanism <18220087@std.stei.itb.ac.id>
2022-06-02 23:10:05 +0700

    item naming done, dummy function added

--- script.js
+++ script.js
@@ -29,159 +29,183 @@ const lv_1 = document.querySelector('.lv.one');
 const lv_2 = document.querySelector('.lv.two');
 const lv_3 = document.querySelector('.lv.three');
 
+function getRndInteger(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) ) + min;
+}
 
 // cloud to gateway
 new LeaderLine(cloud, gateway, {
-    color : 'black',
+    color : getRndInteger(0,1) > 0 ? 'black' : 'green',
     size : 2,
     endPlug : 'behind',
     startSocket : 'top',
-    endSocket : 'top'
+    endSocket : 'top',
+    middleLabel : getRndInteger(0,90) + 'ms'
 });
 
 // gateway to data warehouse
 new LeaderLine(gateway, dataWarehouse, {
-    color : 'black',
+    color : getRndInteger(0,1) > 0 ? 'black' : 'green',
     size : 2,
     endPlug : 'behind',
-    path : 'grid'
+    path : 'grid',
+    middleLabel : getRndInteger(0,90) + 'ms'
 });
 
 // gateway to wifi remote
 new LeaderLine(gateway, remoteWifi, {
-    color : 'black',
+    color : getRndInteger(0,1) > 0 ? 'black' : 'green',
     size : 2,
     endPlug : 'behind',
     endSocket : 'left',
-    path : 'grid'
+    path : 'grid',
+    middleLabel : getRndInteger(0,90) + 'ms'
 });
 
 // internet gateway to labME gateway
 new LeaderLine(gateway, labMEgateway, {
-    color : 'black',
+    color : getRndInteger(0,1) > 0 ? 'black' : 'green',
     size : 2,
-    endPlug : 'behind'
+    endPlug : 'behind',
+    middleLabel : getRndInteger(0,90) + 'ms'
 });
 
 // switch to switch
 new LeaderLine(netSwitchTop, netSwitchBottom, {
-    color : 'black',
+    color : getRndInteger(0,1) > 0 ? 'black' : 'green',
     size : 2,
-    endPlug : 'behind'
+    endPlug : 'behind',
+    middleLabel : getRndInteger(0,90) + 'ms'
 });
 // switch to labME gateway
 new LeaderLine(netSwitchTop, labMEgateway, {
-    color : 'black',
+    color : getRndInteger(0,1) > 0 ? 'black' : 'green',
     size : 2,
-    endPlug : 'behind'
+    endPlug : 'behind',
+    middleLabel : getRndInteger(0,90) + 'ms'
 });
 // gateway to modul lokal
 new LeaderLine(labMEgateway, modulLokal, {
-    color : 'black',
+    color : getRndInteger(0,1) > 0 ? 'black' : 'green',
     size : 2,
     endPlug : 'behind',
     path : 'grid',
-    endSocket : 'top'
+    endSocket : 'top',
+    middleLabel : getRndInteger(0,90) + 'ms'
 });
 // gateway to data lake
 new LeaderLine(labMEgateway, dataLake, {
-    color : 'black',
+    color : getRndInteger(0,1) > 0 ? 'black' : 'green',
     size : 2,
     endPlug : 'behind',
     path : 'grid',
-    endSocket : 'top'
+    endSocket : 'top',
+    middleLabel : getRndInteger(0,90) + 'ms'
 });
 // gateway to wifi
 new LeaderLine(labMEgateway, APWifi, {
-    color : 'black',
+    color : getRndInteger(0,1) > 0 ? 'black' : 'green',
     size : 2,
     endPlug : 'behind',
     path : 'grid',
-    endSocket : 'top'
+    endSocket : 'top',
+    middleLabel : getRndInteger(0,90) + 'ms'
 });
 // wifi remote to pc admin
 new LeaderLine(remoteWifi, AdminPC, {
-    color : 'black',
+    color : getRndInteger(0,1) > 0 ? 'black' : 'green',
     size : 2,
     endPlug : 'behind',
-    dash : true
+    dash : true,
+    middleLabel : getRndInteger(0,90) + 'ms'
 });
 // wifi remote to 3rd party
 new LeaderLine(remoteWifi, thirdPartyPC, {
-    color : 'black',
+    color : getRndInteger(0,1) > 0 ? 'black' : 'green',
     size : 2,
     endPlug : 'behind',
     endSocket : 'top',
-    dash : true
+    dash : true,
+    middleLabel : getRndInteger(0,90) + 'ms'
 });
 // switch to web box
 new LeaderLine(netSwitchBottom, webBox, {
-    color : 'black',
+    color : getRndInteger(0,1) > 0 ? 'black' : 'green',
     size : 2,
     endPlug : 'behind',
-    endSocket : 'top'
+    endSocket : 'top',
+    middleLabel : getRndInteger(0,90) + 'ms'
 });
 // switch to modul relay ethernet
 new LeaderLine(netSwitchBottom, modulRelay, {
-    color : 'black',
+    color : getRndInteger(0,1) > 0 ? 'black' : 'green',
     size : 2,
     endPlug : 'behind',
-    endSocket : 'top'
+    endSocket : 'top',
+    middleLabel : getRndInteger(0,90) + 'ms'
 });
 // switch to modul lokal beban
 new LeaderLine(netSwitchBottom, modulLokalBeban, {
-    color : 'black',
+    color : getRndInteger(0,1) > 0 ? 'black' : 'green',
     size : 2,
     endPlug : 'behind',
-    endSocket : 'top'
+    endSocket : 'top',
+    middleLabel : getRndInteger(0,90) + 'ms'
 });
 // web box to IED hybrid inverter
 new LeaderLine(webBox, IED, {
-    color : 'black',
+    color : getRndInteger(0,1) > 0 ? 'black' : 'green',
     size : 2,
     endPlug : 'behind',
-    endSocket : 'top'
+    endSocket : 'top',
+    middleLabel : getRndInteger(0,90) + 'ms'
 });
 // web box to IED PV Grid inverter
 new LeaderLine(webBox, IED_PV, {
-    color : 'black',
+    color : getRndInteger(0,1) > 0 ? 'black' : 'green',
     size : 2,
     endPlug : 'behind',
-    endSocket : 'top'
+    endSocket : 'top',
+    middleLabel : getRndInteger(0,90) + 'ms'
 });
 // modul lokal beban to smart meter beban listrik
 new LeaderLine(modulLokalBeban, smartMeter, {
-    color : 'black',
+    color : getRndInteger(0,1) > 0 ? 'black' : 'green',
     size : 2,
     endPlug : 'behind',
-    endSocket : 'top'
+    endSocket : 'top',
+    middleLabel : getRndInteger(0,90) + 'ms'
 });
 
 // IED hybrid inverter to hybrid inverter
 new LeaderLine(IED, hybridInverter, {
-    color : 'black',
+    color : getRndInteger(0,1) > 0 ? 'black' : 'green',
     size : 2,
     endPlug : 'behind',
-    endSocket : 'top'
+    endSocket : 'top',
+    middleLabel : getRndInteger(0,90) + 'ms'
 });
 // IED pv grid inverter to pv grid inverter
 new LeaderLine(IED_PV, gridInverter, {
-    color : 'black',
+    color : getRndInteger(0,1) > 0 ? 'black' : 'green',
     size : 2,
     endPlug : 'behind',
-    endSocket : 'top'
+    endSocket : 'top',
+    middleLabel : getRndInteger(0,90) + 'ms'
 });
 // modul relay to LV-1
 new LeaderLine(modulRelay, lv_3, {
-    color : 'black',
+    color : getRndInteger(0,1) > 0 ? 'black' : 'green',
     size : 2,
     endPlug : 'behind',
+    middleLabel : getRndInteger(0,90) + 'ms'
 });
 // smart meter to LV-1
 new LeaderLine(smartMeter, lv_3, {
-    color : 'black',
+    color : getRndInteger(0,1) > 0 ? 'black' : 'green',
     size : 2,
     endPlug : 'behind',
+    middleLabel : getRndInteger(0,90) + 'ms'
 });
 // battery to hybrid inverter (DC)
 new LeaderLine(battery, hybridInverter, {
@@ -189,7 +213,8 @@ new LeaderLine(battery, hybridInverter, {
     size : 2,
     endPlug : 'behind',
     dash : true,
-    middleLabel: 'AC'
+    middleLabel: 'DC',
+    fontSize : 4
 });
 // hybrid inverter to LV-1 (AC)
 new LeaderLine(hybridInverter, lv_2, {
